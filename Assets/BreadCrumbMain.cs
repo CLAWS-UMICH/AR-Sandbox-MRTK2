4e5c692024a24afe07ac0d2fd06ea3fbd1fb7787
btschneid <btschneid3@gmail.com>
2022-10-29 01:04:10 -0400

    Finished the bread crumbs
    Made it so prev position 1 m away creates crumb and on collision it destroys

--- Assets/BreadCrumbMain.cs
+++ Assets/BreadCrumbMain.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class BreadCrumbMain : MonoBehaviour
 {
-    [SerializeField] private bool _buttonStatus;
     [SerializeField] private GameObject crumb;
     [SerializeField] private GameObject parent;
+    [SerializeField] private GameObject camera;
 
+    private bool _buttonStatus;
+    private bool _collided;
     private int pressedNumber;
     private GameObject crumbObject;
-    private int crumCount = 0;
 
+    private Vector3 prevPosition;
+    private Vector3 currentPosition;
+    private GameObject _collisionCrumbObject;
+
+    private float distance;
+
+    // Get Function to see if button is on or off
     public bool ButtonStatus
     {
         get
@@ -19,6 +27,18 @@ public class BreadCrumbMain : MonoBehaviour
             return _buttonStatus;
         }
     }
+
+    // If i want to use another script to destroy the crumb on collision
+    /*
+    public GameObject CollisionTrue
+    {
+        set
+        {
+            _collided = true;
+            _collisionCrumbObject = value;
+        }
+    */
+
 
     // Start is called before the first frame update
     void Start()
@@ -33,22 +53,45 @@ public class BreadCrumbMain : MonoBehaviour
     {
         while (true)
         {
+            // While the button is pressed this runs
             if (_buttonStatus)
             {
-                crumCount++;
-                crumbObject = (GameObject) Instantiate(crumb, transform);
-                crumbObject.name = "c" + crumCount;
+                // Gets current position of camera (player)
+                currentPosition = camera.transform.position;
+
+                // Calculates the distance of current position with previous position
+                distance = Vector3.Distance(currentPosition, prevPosition);
+
+                // If distance is greater than 1, creates a crumb object at the previous position
+                // Then resets the previous position to current position
+                if (distance > 1)
+                {
+                    crumbObject = (GameObject)Instantiate(crumb, prevPosition, Quaternion.identity, transform);
+                    crumbObject.name = "crumb";
+                    prevPosition = currentPosition;
+                }
+
+                // If collided, crumb is broken (only used if we want to use another script)
+                /*
+                if (_collided) 
+                {
+                    Destroy(_collisionCrumbObject);
+                    _collided = false;
+                }
+                */
             }
             yield return new WaitForSeconds(1);
         }
     }
 
+    // Function to set variable if the button is pressed or not
     public void pressed()
     {
         pressedNumber++;
         if (pressedNumber == 1)
         {
             _buttonStatus = true;
+            prevPosition = currentPosition = camera.transform.position;
         }
         else if (pressedNumber == 2)
         {

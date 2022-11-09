404740c765152c2bc94b68bfc49554d2731cf170
Patrick Halim <pnhalim@umich.edu>
2022-11-09 00:57:53 -0500

    Fix astronaut class

--- Assets/AstronautData.cs
+++ Assets/AstronautData.cs
@@ -2,22 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class AstronautData
 {
-  class AstronautData{
-    
-    Stack astrobiodata = new Stack();
     public string height;
     public int age; 
     public string hometown; 
     public string occupation;
- 
-    //using ToString()
-
-    Stack astrobiodata = new Stack();
-    astrobiodata.Push(height);
-
-
-
-  }
 }

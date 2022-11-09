5e7e6b1dda9c2c11bfe93e0e5a1ef21cd666636e
nlnina <91028635+nlnina@users.noreply.github.com>
2022-11-09 13:35:24 -0500

    ninaastropart2

--- Assets/AstronautData.cs
+++ Assets/AstronautData.cs
@@ -7,17 +7,51 @@ public class NewBehaviourScript : MonoBehaviour
   class AstronautData{
     
     Stack astrobiodata = new Stack();
+    public string firstName;
+    public string lastName; 
+    public int ID; 
     public string height;
     public int age; 
     public string hometown; 
     public string occupation;
+    public string marriageStatus; 
  
     //using ToString()
+    public string ID = ID.ToString();
+    public string height = height.ToString();
+    public string age = age.ToString();
 
-    Stack astrobiodata = new Stack();
-    astrobiodata.Push(height);
+    Console.WriteLine("enter First Name:");
+    firstName = Console.ReadLine();
+    Console.WriteLine("First name is: " + firstName);
 
+    Console.WriteLine("enter Last Name:");
+    lastName = Console.ReadLine();
+    Console.WriteLine("Last name is: " + lastName);
 
+    Console.WriteLine("enter ID:");
+    ID = Console.ReadLine();
+    Console.WriteLine("ID is: " + ID);
+
+    Console.WriteLine("enter height:");
+    height = Console.ReadLine();
+    Console.WriteLine("Height is: " + height);  
+
+    Console.WriteLine("enter age:");
+    age = Console.ReadLine();
+    Console.WriteLine("Age is: " + age); 
+
+    Console.WriteLine("enter hometown:");
+    hometown = Console.ReadLine();
+    Console.WriteLine("Hometown is: " + hometown); 
+
+    Console.WriteLine("enter occupation:");
+    occupation = Console.ReadLine();
+    Console.WriteLine("Occupation is: " + occupation); 
+
+    Console.WriteLine("enter marriageStatus:");
+    marriageStatus = Console.ReadLine();
+    Console.WriteLine("marriageStatus is: " + marriageStatus); 
 
   }
 }

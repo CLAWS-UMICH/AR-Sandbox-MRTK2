b1d77e7b4218731bda9f36d4c8c2f56ed7faf334
zedong <zedong@umich.edu>
2022-11-14 14:13:44 -0500

    update2

--- Assets/PositionOfMessage.cs
+++ Assets/PositionOfMessage.cs
@@ -3,17 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public static class PositionOfMessage
+public class PositionOfMessage : MonoBehaviour
 {
 
-    // if window is at (0,0,0)
-    [SerializeField] static float x = 0.31F;
-    [SerializeField] static float y = 0.22F;
-    [SerializeField] static float z = 0.00F;
-    [SerializeField] static float heightOfBox = 0.12F;
+    private static float x;
+    private static float y;
+    private static float z;
+    private static float heightOfBox = 0.12F;
 
+    [SerializeField] GameObject window;
     
-    public static Vector3 getPosition() {
+    // the script is not enabled (used in onclick()), 
+    // so we should use awake instead of start?
+    void Awake() {
+        // Set up the first position of the message box basing on the position of the window
+        Transform t  = transform;
+        Vector3 pos = t.position;
+        x = pos[0] + 0.2F;
+        y = pos[1] + 0.1F;
+        z = pos[2];
+    }
+
+    public Vector3 getPosition() {
         y -= heightOfBox;
         return new Vector3(x, y, z);
     }

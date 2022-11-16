e1117f01dae9f53db7b8e31cdf6b2dacda307d33
adhav <uniqname@umich.edu>
2022-11-16 11:48:04 -0500

    worked on map

---
+++ Assets/CursorController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CursorController : MonoBehaviour
+{
+    [SerializeField] GameObject g;
+    [SerializeField] GameObject camera;
+    Vector3 NorthDir;
+    int x = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x++;
+        NorthDir.z = camera.transform.eulerAngles.y;
+        g.transform.localRotation =Quaternion.Euler(NorthDir.z + 120, 90, -90);
+        g.transform.localPosition = new Vector3((float)0.01*camera.transform.position[0], (float)0.01*camera.transform.position[2], (float)-.001);
+        Debug.Log(100*camera.transform.rotation[1]);   
+    }
+    // IEnumerator UpdateCursor() {
+    //     while(true) {
+    //         camera.getComponent
+    //     }
+    // }
+}

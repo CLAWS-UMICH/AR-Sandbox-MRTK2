7341fa17a99a18e3967035fbdae883b940078f21
adhav <uniqname@umich.edu>
2022-11-22 15:11:02 -0500

    fixed buggy cursor

--- Assets/CursorController.cs
+++ Assets/CursorController.cs
@@ -22,7 +22,6 @@ public class CursorController : MonoBehaviour
         NorthDir.z = camera.transform.eulerAngles.y;
         g.transform.localRotation =Quaternion.Euler(NorthDir.z + 120, 90, -90);
         g.transform.localPosition = new Vector3((float)0.01*camera.transform.position[0], (float)0.01*camera.transform.position[2], (float)-.001);
-        Debug.Log(100*camera.transform.rotation[1]);   
     }
     // IEnumerator UpdateCursor() {
     //     while(true) {

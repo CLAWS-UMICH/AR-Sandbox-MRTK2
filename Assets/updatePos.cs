4b6fa90d43f9565e720a6de1cff77b0774377b97
zedong <zedong@umich.edu>
2022-11-29 12:34:12 -0500

    a

--- Assets/updatePos.cs
+++ Assets/updatePos.cs
@@ -8,14 +8,16 @@ public class updatePos : MonoBehaviour
     public GameObject g;
     Coroutine c;
     private manipulate m = new manipulate();
-    public float delay = 0.01f;
+    public float delay = 0.00001f;
     public float x;
     public float y;
-    public float z = 2f;
+    public float z = 0.5f;
     double angleXPrev = 0;
     double angleYPrev = 0;
-    Vector3 curPosition = new Vector3(0,0,0);
+    Vector3 curPosition = new Vector3(0,0,0.4f);
     Vector3 positionDiff = new Vector3(0,0,0);
+    Vector3 angleDeviation = new Vector3(0,0,0);
+
 
     // Start is called before the first frame update
     void Start()
@@ -37,6 +39,7 @@ public class updatePos : MonoBehaviour
             if (m.getMode()) {
                 Vector3 angles = Camera.main.transform.eulerAngles;
             
+                // let the screen always face with the camera(user)
                 g.GetComponent<Transform>().eulerAngles = Camera.main.transform.eulerAngles;
                 // converting value to radians
                 double angleX = (angles[1] * (Math.PI)) / 180;
@@ -54,22 +57,39 @@ public class updatePos : MonoBehaviour
                 // Debug.Log("y" + Math.Sin(angleY) + " " + angles[0]);
                 // Debug.Log("z" + z - (float)(a * Math.Tan(angleX)));
                 
-                g.GetComponent<Transform>().position = Camera.main.transform.position + new Vector3((z + positionDiff[0]) * (float)(Math.Sin(angleX)), (-z - positionDiff[1]) * (float)(Math.Sin(angleY)), z * (float)(Math.Cos(angleX)));
+                // g.GetComponent<Transform>().position = Camera.main.transform.position + new Vector3((z + positionDiff[0]) * (float)(Math.Sin(angleX)) + positionDiff[0], (-z - positionDiff[1]) * (float)(Math.Sin(angleY)) - positionDiff[1], z * (float)(Math.Cos(angleX + angleY)));
+                // z = curPosition[2];
+                g.GetComponent<Transform>().position = Camera.main.transform.position + new Vector3((z) * (float)(Math.Sin(angleX + angleDeviation[0])), (-z) * (float)(Math.Sin(angleY - angleDeviation[1])), z * (float)(Math.Cos(angleX + angleY)));
                 // g.GetComponent<Transform>().position = Camera.main.transform.position + new Vector3(z * (float)(Math.Sin(angleX)) + positionDiff[0], -z * (float)(Math.Sin(angleY) - positionDiff[1]), z * (float)(Math.Cos(angleX)));
                 
                 // position before manipulation
-                curPosition = g.GetComponent<Transform>().position;
+                // curPosition = Camera.main.transform.position;
                 // g.GetComponent<Transform>().position = Camera.main.transform.position + new Vector3(z * (float)(Math.Sin(angleX)), -z * (float)(Math.Sin(angleY)), z * (float)(Math.Cos(angleY)));
 
-                // Debug.Log(z * (float)(Math.Sin(angleX)) + " " + -z * (float)(Math.Sin(angleY)) + " " + z * (float)(Math.Cos(angleX)));
+                // Debug.Log(g.GetComponent<Transform>().position);
+                // Debug.Log((Math.Sin(angleY)) + " " + angles[0] + " " + (-z) * (float)(Math.Sin(angleY)));
 
                 // g.GetComponent<Transform>().rotation = Camera.main.transform.rotation;
                 
                 
             }
             else {
-                // if user is manipulating the window, get the diff between new position and the position before manipulation
-                positionDiff = g.GetComponent<Transform>().position - curPosition;
+                // if user is manipulating the window, get the diff between new position and the position of camera
+                positionDiff = g.GetComponent<Transform>().position - Camera.main.transform.position;
+                // 0 - 0.7 radian = (0 - 40 degree)
+                // Calculate the angle deviation between camera and window
+                float deviationX = (float)Math.Asin(positionDiff[0] / 0.5f);
+                float deviationY = (float)Math.Asin(positionDiff[1] / 0.5f);
+                Debug.Log(deviationX);
+                // Set bounds, user is not allowed to move the window out of bound
+                if (deviationX > 1.4f || Double.IsNaN(deviationX)) deviationX = 0.7f;
+                if (deviationX < -1.4f || Double.IsNaN(deviationX)) deviationX = -0.7f;
+                if (deviationY > 1.4f || Double.IsNaN(deviationY)) deviationY = 0.7f;
+                if (deviationY < -1.4f || Double.IsNaN(deviationY)) deviationY = -0.7f;
+                angleDeviation[0] = deviationX;
+                angleDeviation[1] = deviationY;
+                
+
             }
             yield return new WaitForSeconds(delay);
         }

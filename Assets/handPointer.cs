318cc1eccd99dab15f8eaa7d3083e3d004921445
selinal <selnal@umich.edu>
2022-11-30 11:35:17 -0500

    swipe msg

--- Assets/handPointer.cs
+++ Assets/handPointer.cs
@@ -6,33 +6,47 @@ public class handPointer : MonoBehaviour
 {
     void Update()
     {
-        double swipeDistance = 0.5;
+        double swipeDistance = 0.2;
         foreach(var source in MixedRealityToolkit.InputSystem.DetectedInputSources)
         {
             if (source.SourceType == Microsoft.MixedReality.Toolkit.Input.InputSourceType.Hand)
             {
                 foreach (var p in source.Pointers)
                 {
-                    if (p is IMixedRealityNearPointer)
-                    {
-                        // Ignore near pointers, we only want the rays
-                        continue;
-                    }
+                    // if (p is IMixedRealityNearPointer)
+                    // {
+                    //     // Ignore near pointers, we only want the rays
+                    //     continue;
+                    // }
                     if (p.Result != null)
                     {
-                        var startPoint = p.Position;
-                        var endPoint = p.Result.Details.Point;
+                        var startPointy = p.Position[1];
+                        var endPointy = p.Result.Details.Point[1];
+                        var startPointx = p.Position[0];
+                        var endPointx = p.Result.Details.Point[0];
                         var hitObject = p.Result.Details.Object;
                         if (hitObject)
                         {
-                            if (startPoint != endPoint)
+                            // Debug.Log($"startPos: {startPoint}");
+                            // Debug.Log($"endPos: {endPoint}");
+                            if (startPointy + swipeDistance >= endPointy)
                             {
-                                Debug.Log($"startPos: {startPoint}");
-                                Debug.Log($"endPos: {endPoint}");
+                                Debug.Log("Swiped Up");
+                            }
+                            if (startPointy + swipeDistance <= endPointy)
+                            {
+                                Debug.Log("Swiped Down");
+                            }
+                            if (startPointx + swipeDistance >= endPointx)
+                            {
+                                Debug.Log("Swiped Right");
+                            }
+                            if (startPointx + swipeDistance <= endPointx)
+                            {
+                                Debug.Log("Swiped Left");
                             }
                         }
                     }
-
                 }
             }
         }

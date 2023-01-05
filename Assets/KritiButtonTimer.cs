d4478b57929440c7423ad0df7869a3c52aa93f88
Joel <joelyson@DESKTOP-VLF4QBF.localdomain>
2023-01-04 19:43:00 -0500

    Add bigScreen

--- Assets/KritiButtonTimer.cs
+++ Assets/KritiButtonTimer.cs
@@ -7,8 +7,10 @@ public class KritiButtonTimer : MonoBehaviour
     // Start is called before the first frame update
     private IEnumerator coroutine;
     public GameObject panel;
+    public GameObject panel2;
 
     public AudioClip clip;
+    bool active = false;
    
     void Start()
     {
@@ -27,7 +29,14 @@ public class KritiButtonTimer : MonoBehaviour
     private IEnumerator UpdateTime(){
         while (true){
             yield return new WaitForSeconds(2);
-            panel.SetActive(true);
+            if(active){
+                active = false;
+            }
+            else{
+                active = true;
+            }
+            panel.SetActive(active);
+            panel2.SetActive(active);
             Debug.Log("hi");
             gameObject.GetComponent<AudioSource>().PlayOneShot(clip);
         }

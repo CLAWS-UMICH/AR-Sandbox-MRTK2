95ba66e2971dc4c5c7ee9c887a9fd00bc62d7571
supraditya <aditya300100@gmail.com>
2023-10-07 17:55:55 -0400

    Timer task complete

--- Assets/2023-24/Week2/Aditya Menon/TimerScript.cs
+++ Assets/2023-24/Week2/Aditya Menon/TimerScript.cs
@@ -5,9 +5,12 @@ using TMPro;
 
 public class TimerScript : MonoBehaviour
 {
-    int count = 0;
+    int secondsCount = 0;
+    int minutesCount = 0;
+    int hoursCount = 0;
     GameObject screen;
     TextMeshPro textMeshPro;
+    bool isTimerRunning = false;
     private IEnumerator timerCoroutine;
     // Start is called before the first frame update
     void Start()
@@ -16,33 +19,86 @@ public class TimerScript : MonoBehaviour
         textMeshPro = screen.transform.Find("Text").gameObject.GetComponent<TextMeshPro>();
         textMeshPro.text = "HH:MM:SS";
         timerCoroutine = _UpdateTimer();
-}
+    }
     public void StartTimer()
     {
-       StartCoroutine(timerCoroutine);
+        if(isTimerRunning==false)
+        {
+            StartCoroutine(timerCoroutine);
+            isTimerRunning = true;
+        }
     }
     public void StopTimer()
     {
-       StopCoroutine(timerCoroutine);
+        StopCoroutine(timerCoroutine);
+        isTimerRunning = false;
     }
     public void ResetTimer()
     {
-        StopCoroutine(timerCoroutine);
-        count = 0;
-        textMeshPro.text = count.ToString();
+        StopTimer();
+        secondsCount = 0;
+        minutesCount = 0;
+        hoursCount = 0;
+        textMeshPro.text = "00:00:00";
     }
     IEnumerator _UpdateTimer()
     {
         while(true)
         {
             yield return new WaitForSeconds(1.0f);
-            count++;
-            UpdateUI();
+            textMeshPro.text = UpdateCount();
         }
     }
-    void UpdateUI()
+    string UpdateCount()
     {
-        textMeshPro.text = count.ToString();
+        if(minutesCount==59 && secondsCount==59)
+        {
+            hoursCount++;
+            minutesCount = 0;
+            secondsCount = 0;
+        }
+        else if (secondsCount == 59)
+        {
+            minutesCount++;
+            secondsCount = 0;
+        }
+        else
+        {
+            secondsCount++;
+        }
+        string counter = "";
+        string hoursString = "";
+        string minutesString = "";
+        string secondsString = "";
+        if(hoursCount < 10)
+        {
+            hoursString = "0" + hoursCount.ToString();
+        }
+        else
+        {
+            hoursString = hoursCount.ToString();
+        }
+
+        if(minutesCount < 10)
+        {
+            minutesString = "0" + minutesCount.ToString();
+        }
+        else
+        {
+            minutesString = minutesCount.ToString();
+        }
+
+        if (secondsCount < 10)
+        {
+            secondsString = "0" + secondsCount.ToString();
+        }
+        else
+        {
+            secondsString = secondsCount.ToString();
+        }
+        counter = hoursString + ":" + minutesString + ":" + secondsString;
+        return counter;
     }
+  
 
 }

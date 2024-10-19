923fa5f85b0ef659867b6c1c3af303ce3cc33b9a
Haotian Li <68355398+Fauq@users.noreply.github.com>
2024-10-19 13:37:08 -0400

    ui for vitals

--- Assets/2024-25/Week-4-5/Vitals/VitalsController.cs
+++ Assets/2024-25/Week-4-5/Vitals/VitalsController.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro; 
 public class VitalsController : MonoBehaviour
 {
     private Subscription<VitalsUpdatedEvent> vitalsUpdatedEvent;
+    TextMeshPro heartRateText;
+    TextMeshPro bloodPressureText;
+    TextMeshPro oxygenLevelText;
+    TextMeshPro suitTempText;
+    GameObject screen;
     // Start is called before the first frame update
     void Start()
     {
         // Subscribe to the events
         vitalsUpdatedEvent = EventBus.Subscribe<VitalsUpdatedEvent>(OnVitalsUpdatedEvent);
+        screen = GameObject.Find("screen");
+        heartRateText = screen.transform.Find("heartRate").gameObject.GetComponent<TextMeshPro>();
+        bloodPressureText = screen.transform.Find("bloodPressure").gameObject.GetComponent<TextMeshPro>();
+        oxygenLevelText = screen.transform.Find("oxygenLevel").gameObject.GetComponent<TextMeshPro>();
+        suitTempText = screen.transform.Find("suitTemp").gameObject.GetComponent<TextMeshPro>();
     }
     void OnDestroy()
     {
@@ -22,5 +33,10 @@ public class VitalsController : MonoBehaviour
     {
         Vitals vitals = e.vitals; 
         // Update the UI to reflect the new vitals values
+        heartRateText.text = "Heart Rate: " + vitals.heart_rate;
+        bloodPressureText.text = "Blood Pressure: " + vitals.blood_pressure;
+        oxygenLevelText.text = "Oxygen Level: " + vitals.oxygen;
+        suitTempText.text = "Suit Temp: " + vitals.suit_temp;
+
     }
 }

af9d269a893eb8bce3761f54511177d37b719334
Ian Kim <ianhkim@umich.edu>
2024-10-29 23:05:25 -0400

    Ian/Haotian Minifeature (Vitals)
    just might be the best vitals UI to exist

--- Assets/2024-25/Week-4-5/Vitals/VitalsController.cs
+++ Assets/2024-25/Week-4-5/Vitals/VitalsController.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro; 
+using TMPro;
+using UnityEngine.UI;
 public class VitalsController : MonoBehaviour
 {
     private Subscription<VitalsUpdatedEvent> vitalsUpdatedEvent;
@@ -10,17 +11,44 @@ public class VitalsController : MonoBehaviour
     TextMeshPro oxygenLevelText;
     TextMeshPro suitTempText;
     GameObject screen;
+
+    GameObject canvas;
+    public Button heartRateButton;
+    public Button bloodPressureButton;
+    public Button oxygenLevelButton;
+    public Button suitTemptButton;
+
     // Start is called before the first frame update
     void Start()
     {
         // Subscribe to the events
         vitalsUpdatedEvent = EventBus.Subscribe<VitalsUpdatedEvent>(OnVitalsUpdatedEvent);
+        Debug.Log("subscribed");
+
         screen = GameObject.Find("screen");
         heartRateText = screen.transform.Find("heartRate").gameObject.GetComponent<TextMeshPro>();
         bloodPressureText = screen.transform.Find("bloodPressure").gameObject.GetComponent<TextMeshPro>();
         oxygenLevelText = screen.transform.Find("oxygenLevel").gameObject.GetComponent<TextMeshPro>();
         suitTempText = screen.transform.Find("suitTemp").gameObject.GetComponent<TextMeshPro>();
+
+        canvas = GameObject.Find("Canvas");
+        heartRateButton = canvas.transform.Find("LogHeart").gameObject.GetComponent<Button>();
+        bloodPressureButton = canvas.transform.Find("LogOx").gameObject.GetComponent<Button>();
+        oxygenLevelButton = canvas.transform.Find("LogSuit").gameObject.GetComponent<Button>();
+        suitTemptButton = canvas.transform.Find("LogBP").gameObject.GetComponent<Button>();
+
+        heartRateButton.onClick.AddListener(LogRate);
+        bloodPressureButton.onClick.AddListener(LogPressure);
+        oxygenLevelButton.onClick.AddListener(LogOxygen);
+        suitTemptButton.onClick.AddListener(LogTempt);
+
+        // Added click listeners that call the parameter function
+
+
+        Debug.Log("connected");
+
     }
+
     void OnDestroy()
     {
         // Unsubscribe when the script is destroyed
@@ -31,12 +59,34 @@ public class VitalsController : MonoBehaviour
     }
     private void OnVitalsUpdatedEvent(VitalsUpdatedEvent e)
     {
+        Debug.Log("vitals received");
+
         Vitals vitals = e.vitals; 
         // Update the UI to reflect the new vitals values
         heartRateText.text = "Heart Rate: " + vitals.heart_rate;
         bloodPressureText.text = "Blood Pressure: " + vitals.blood_pressure;
         oxygenLevelText.text = "Oxygen Level: " + vitals.oxygen;
         suitTempText.text = "Suit Temp: " + vitals.suit_temp;
-
     }
+
+    /* If you're reading this, just know that the python server did not want to communicate with Unity whatsoever.
+    This was one of the most mind numbing experiences I've ever had in my life. I told my partner I would be done by 9:00PM, but it is
+    currently 10:56PM. I think it was a problem with the firewall, because I had to go into my Windows environment settings just to
+    get the python server started. Holy crap. I spent so much time trying to fix it. You'll see random print and debug statements
+    all over the place because that was me trying to figure out what was going on. What a disaster.*/
+
+    public void LogRate() {
+        Debug.Log(heartRateText);
+    }
+    public void LogPressure() {
+        Debug.Log(bloodPressureText);
+    }
+    public void LogOxygen() {
+        Debug.Log(oxygenLevelText);
+    }
+    public void LogTempt() {
+        Debug.Log(suitTempText);
+    }
+
+    // Logs the current value for each vital in the console
 }
--- Assets/2024-25/Week-4-5/Vitals/VitalsServer.py
+++ Assets/2024-25/Week-4-5/Vitals/VitalsServer.py
@@ -8,10 +8,12 @@ import asyncio
 import websockets
 import json
 import random  # Import the random module to generate random values
+print("executed")
 
 async def send_random_data(websocket):
     while True:
         # Create a JSON object with random values
+        print("created")
         data = {
             "id": "1",
             "type": "Vitals",
@@ -26,20 +28,26 @@ async def send_random_data(websocket):
 
         # Convert the data to a JSON string
         message = json.dumps(data)
+        print("sending vitals")
 
         # Send the JSON message to the connected client (Unity)
         await websocket.send(message)
+        print("message sent")
 
         # Wait for one second before sending the next JSON
         await asyncio.sleep(3)
+        
 
 async def handle_client(websocket, path):
     try:
         await send_random_data(websocket)
     except websockets.exceptions.ConnectionClosed:
+        print("closed")
         pass
 
 start_server = websockets.serve(handle_client, "localhost", 8080)
+print("server made")
 
 asyncio.get_event_loop().run_until_complete(start_server)
+print("server running")
 asyncio.get_event_loop().run_forever()
